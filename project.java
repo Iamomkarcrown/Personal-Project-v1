de8b6f7bff68cdecb7a2af597ac043b9e517805b
Iamomkarcrown <omkaware750@gmail.com>
2025-08-22 08:56:46 -0700

    added new line for CI/CD purpose

--- project.java
+++ project.java
@@ -1,5 +1,7 @@
 public class project{
     public static void main(String[] args){
         System.out.println("Hello Doston!");
+        System.out.println("Hi Lets create one project for ci/cd Demo");
+
     }
 }
